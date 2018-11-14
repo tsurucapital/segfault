ba0a014202593804dc7c587b787ffdb5f2604bd7
Takano Akio <tak@anoak.io>
2018-11-14 04:37:23 +0000

    monomorphize

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wall #-}
 
 import Data.ByteString (ByteString, packCStringLen)
 
@@ -39,20 +40,20 @@ readFromPtr buf = do
     print bs -- required
     return bs
 
-type Enumerator s m a = Iteratee s m a -> m (Iteratee s m a)
+type Enumerator s a = Iteratee s a -> IO (Iteratee s a)
 
-newtype Iteratee s m a = Iteratee { runIter :: forall r.
-          (a -> s -> m r) ->
-          ((s -> Iteratee s m a) -> m r) ->
-          m r}
+newtype Iteratee s a = Iteratee { runIter :: forall r.
+          (a -> s -> IO r) ->
+          ((s -> Iteratee s a) -> IO r) ->
+          IO r}
 
-enumFromCallbackCatch :: (Monad m, Monoid s) => m s -> Enumerator s m a
+enumFromCallbackCatch :: (Monoid s) => IO s -> Enumerator s a
 enumFromCallbackCatch c = loop
   where
     loop iter = runIter iter (\a b -> return $ idone a b) onCont
     onCont k = c >>= loop . k
 
-idone :: Monad m => a -> s -> Iteratee s m a
+idone :: a -> s -> Iteratee s a
 idone a s = Iteratee $ \onDone _ -> onDone a s
 
 eneeCheckIfDone f inner = Iteratee $ \od oc ->
@@ -60,34 +61,34 @@ eneeCheckIfDone f inner = Iteratee $ \od oc ->
       onCont k = runIter (f k) od oc
   in runIter inner onDone onCont
 
-type Enumeratee sFrom sTo m a = Iteratee sTo m a -> Iteratee sFrom m (Iteratee sTo m a)
+type Enumeratee sFrom sTo a = Iteratee sTo a -> Iteratee sFrom (Iteratee sTo a)
 
-liftI :: Monad m => (s -> Iteratee s m a) -> Iteratee s m a
+liftI :: (s -> Iteratee s a) -> Iteratee s a
 liftI k = Iteratee $ \_ onCont -> onCont k
 
-decodePcap :: Enumeratee ByteString [(TimeOfDay, ByteString)] IO a
+decodePcap :: Enumeratee ByteString [(TimeOfDay, ByteString)] a
 decodePcap = eneeCheckIfDone (liftI . go)
     where
         go k c = eneeCheckIfDone (liftI . go) (k [(TimeOfDay 0, c)])
 
-lift :: (Monoid s, Monad m) => m a -> Iteratee s m a
+lift :: (Monoid s) => IO a -> Iteratee s a
 lift m = Iteratee $ \onDone _ -> m >>= flip onDone (mempty)
 
-instance (Functor m, Monad m) => Functor (Iteratee s m) where
+instance Functor (Iteratee s) where
   fmap f m = Iteratee $ \onDone onCont ->
     let od = onDone . f
         oc = onCont . (fmap f .)
     in runIter m od oc
 
-instance (Functor m, Monad m, Monoid s) => Applicative (Iteratee s m) where
+instance (Monoid s) => Applicative (Iteratee s) where
     pure x  = idone x mempty
     m <*> a = m >>= flip fmap a
 
-instance (Monad m, Monoid s) => Monad (Iteratee s m) where
+instance (Monoid s) => Monad (Iteratee s) where
   return x = Iteratee $ \onDone _ -> onDone x (mempty)
   (>>=) = bindIteratee
 
-bindIteratee :: (Monad m, Monoid s) => Iteratee s m a -> (a -> Iteratee s m b) -> Iteratee s m b
+bindIteratee :: (Monoid s) => Iteratee s a -> (a -> Iteratee s b) -> Iteratee s b
 bindIteratee = self
     where
         self m f = Iteratee $ \onDone onCont ->
@@ -95,7 +96,7 @@ bindIteratee = self
                    where f_cont k = runIter (k stream) onDone onCont
              in runIter m m_done (onCont . (flip self f .))
 
-mapMI_ :: Monad m => (el -> m b) -> Iteratee [el] m ()
+mapMI_ :: (el -> IO b) -> Iteratee [el] ()
 mapMI_ f = liftI step
   where
     step [] = liftI step -- required
